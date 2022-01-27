0e8957234c262ddac52bd341c11eebe2d635e57b
Craig Young <craig.young@pieinsurance.com>
2022-01-27 13:48:51 -0700

    Calculate the julian date. Confirmed these values are correct.

---
+++ PlutoTime.Domain/DateTimeOffsetExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PlutoTime.Domain
+{
+	public static class DateTimeOffsetExtensions
+	{
+		/// <summary>
+		/// Calculates the current Julian Tie
+		/// </summary>
+		/// <remarks>
+		/// </remarks>
+		/// <param name="dateTimeOffset"></param>
+		/// <returns></returns>
+		public static double GetJulianDate(this DateTimeOffset dateTimeOffset)
+		{
+			// 2440587.5 days + UNIX TIME in days === Julian Day
+			var unixTimeSeconds = dateTimeOffset.ToUnixTimeMilliseconds();
+			var unixDays = TimeSpan.FromMilliseconds(unixTimeSeconds).TotalDays;
+			return unixDays + 2440587.5;
+		}
+	}
+}
